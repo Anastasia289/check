80fa8814f8139310a4fd3bbee9463cf0fd99afad
Anastasia <bogdanovanastja@yandex.ru>
2022-11-23 16:54:33 +0300

    написали первую часть

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] ShortWords (string [] a)
+{
+    string [] ShortWords = new string [a.Length];
+    int ii = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        string j = a[i];
+        if (j.Length <= 3 ) 
+        {
+            ShortWords[ii] = j;
+            ii++;
+        }  
+    }
+    return ShortWords;
+}
+
+void PrintArr (string [] arr)
+{
+       for (int i=0; i<arr.Length; i++)
+    {
+            Console.Write(arr[i] + " " );
+    }
+  Console.WriteLine();
+}
+
+Console.WriteLine("Введите предложение");
+string str = Console.ReadLine();
+string [] words = str.Split(' ');
+string [] NewWords = ShortWords (words);
+PrintArr (NewWords);
